f3ad4ecc95783400b06421edb2ee3ef0e564a786
Brett Gillund <brettgillund2@gmail.com>
2022-07-09 22:09:42 -0500

    updating html and JS for date.

---
+++ Develop/JavaScript/JavaScript.js
@@ -0,0 +1,29 @@
+// get date functioning in header
+
+function currentDay() {
+    $('#currentDay').text(moment().format('dddd MMMM Do, YYYY'))
+}
+
+currentDay();
+
+// create the layout of time in html.
+
+// when the application loads the current date loads.
+
+// create hourly time blocks.
+
+// make past timeblocks grey using .past class.
+
+// make curremt timeblock red using .present class.
+
+// make future timeblocks green using future class.
+
+// make the time blocks functional so an event can be entered into them.
+
+// create a save button for each timeblock that saves the event data to local storage.
+
+// when the page refreshes the timeblocks data remains in the block.
+
+
+
+
