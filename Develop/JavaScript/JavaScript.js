e8a7744af6ebd6fba659bf984292ab70f7c66dd9
Brett Gillund <brettgillund2@gmail.com>
2022-07-19 21:21:52 -0500

    getting js to save input

--- Develop/JavaScript/JavaScript.js
+++ Develop/JavaScript/JavaScript.js
@@ -1,24 +1,74 @@
 // get date functioning in header
+// when the application loads the current date loads.
+
+// var form = $('form');
+// var past = new Date()
+// var saveBtn = $('.saveBtn').attr('id');
+
+// console.log(saveBtn);
+
+
+
+// $(document).ready(() => {
+//     $('.saveBtn').on('click', (event) => {
+//         event.preventDefault();
+//         var txt = $('[type=text]').val();
+//     var hour = $(`.hour`).attr('id');
+//     localStorage.setItem(hour, txt);
+//     var savedText = $('.description').attr('id');
+//     // savedText.textContents = (txt);
+//         console.log(txt);
+//         console.log(hour);
+//         console.log(savedText);
+
+
+//         // console.log(localStorage.getItem(hour));
+//         if (localStorage.getItem(hour)) {
+//             alert(`you have saved ${txt} at ${hour}`);
+//         };
+//     })
+
+// });
 
 function currentDay() {
-    $('#currentDay').text(moment().format('dddd MMMM Do, YYYY'))
-}
+    $('#currentDay').text(moment().format('dddd MMMM Do, YYYY'));
+    var currentTime = moment().hours();
+    $('.hour').each(function(){
+        var listHours = parseInt($(this).attr('id').substr(0));
+        // console.log(listHours);
+    })
+    // if (listHours)
+};
+
+// if else checks checking current hour with listHours. 
 
 currentDay();
 
 // create the layout of time in html.
 
-// when the application loads the current date loads.
-
 // create hourly time blocks.
 
-// make past timeblocks grey using .past class.
+
+
+
+
+function renderLastEntry () {
+    var textSaved = $($(this).attr('id'));
+}
+
+
+
+// make past timeblocks grey using .past class. use the var.addClass('past')
 
 // make curremt timeblock red using .present class.
 
-// make future timeblocks green using future class.
+// make future timeblocks green using .future class.
 
 // make the time blocks functional so an event can be entered into them.
+
+
+
+// console.log(form);
 
 // create a save button for each timeblock that saves the event data to local storage.
 
@@ -26,4 +76,6 @@ currentDay();
 
 
 
+
+// create var for each row.
 
