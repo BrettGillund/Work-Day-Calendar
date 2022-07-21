0ffb67dbe59e16823b63dfc3aaf6a9bcda45a3f4
Brett Gillund <brettgillund2@gmail.com>
2022-07-20 19:00:50 -0500

    updating html to target input text and hour value

--- Develop/JavaScript/JavaScript.js
+++ Develop/JavaScript/JavaScript.js
@@ -1,34 +1,55 @@
 // get date functioning in header
 // when the application loads the current date loads.
 
-// var form = $('form');
-// var past = new Date()
-// var saveBtn = $('.saveBtn').attr('id');
+var form = $('form');
+var past = new Date()
+var saveBtn = $('.saveBtn');
+var inputText = $('.textarea');
 
 // console.log(saveBtn);
+// console.log(inputText);
 
 
 
-// $(document).ready(() => {
-//     $('.saveBtn').on('click', (event) => {
-//         event.preventDefault();
-//         var txt = $('[type=text]').val();
-//     var hour = $(`.hour`).attr('id');
-//     localStorage.setItem(hour, txt);
-//     var savedText = $('.description').attr('id');
-//     // savedText.textContents = (txt);
-//         console.log(txt);
-//         console.log(hour);
-//         console.log(savedText);
+$(document).ready(() => {
+    $(saveBtn).on('click', (event) => {
+        event.preventDefault();
+        // console.log(saveBtn);
+        var txt = ($(event.currentTarget).siblings('input').val())
+        // var txt = $('[type=text]').val();
+        // console.log($(event.currentTarget).siblings('input').val())
+        // console.log(event)
+        // console.log(inputText.value)
+    var hour = ($(event.currentTarget).siblings('label').attr('id'))
+    // var hour = $(`label.hour`).attr('id');
+    localStorage.setItem(hour, txt);
+        console.log(hour);
 
 
-//         // console.log(localStorage.getItem(hour));
-//         if (localStorage.getItem(hour)) {
-//             alert(`you have saved ${txt} at ${hour}`);
-//         };
-//     })
+        // console.log(localStorage.getItem(hour));
+        if (localStorage.getItem(hour)) {
+            alert(`you have saved ${txt} at ${hour}`);
+        };
+    })
 
-// });
+function checkTime() {
+    //if check to check the current time versus the time associated with the time of the 
+    // select with jquery (ID or class) dynamically add a class to the sibilings.
+    // set intveral and set timeout.
+}
+
+$('.hour').val(localStorage.getItem('#9AM'));
+$('.hour').val(localStorage.getItem('#10AM'));
+$('.hour').val(localStorage.getItem('#11AM'));
+$('.hour').val(localStorage.getItem('#12AM'));
+$('.hour').val(localStorage.getItem('#13PM'));
+$('.hour').val(localStorage.getItem('#14PM'));
+$('.hour').val(localStorage.getItem('#15PM'));
+$('.hour').val(localStorage.getItem('#16PM'));
+$('.hour').val(localStorage.getItem('#17PM'));
+$('.hour').val(localStorage.getItem('#18PM'));
+
+});
 
 function currentDay() {
     $('#currentDay').text(moment().format('dddd MMMM Do, YYYY'));
@@ -49,12 +70,9 @@ currentDay();
 // create hourly time blocks.
 
 
-
-
-
-function renderLastEntry () {
-    var textSaved = $($(this).attr('id'));
-}
+// function renderLastEntry () {
+//     var textSaved = $($(this).attr('id'));
+// }
 
 
 
