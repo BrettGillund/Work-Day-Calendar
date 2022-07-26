70f62c624b28f2388751ee180915c4723056c37f
Brett Gillund <brettgillund2@gmail.com>
2022-07-26 16:08:06 -0500

    final commit for submission

--- Develop/JavaScript/JavaScript.js
+++ Develop/JavaScript/JavaScript.js
@@ -9,31 +9,31 @@ var inputText = $('.textarea');
 
 
 $(document).ready(() => {
-    $(saveBtn).on('click', (event) => {
+    $(saveBtn).on('click', (event) => { // event listener for each save button.
         event.preventDefault();
         const txt = ($(event.currentTarget).siblings('input').val())
         const hour = ($(event.currentTarget).siblings('input').attr('id'))
-        localStorage.setItem(hour, txt);
+        localStorage.setItem(hour, txt); // saves entered text to local storage.
 
-        if (localStorage.getItem(hour)) {
+        if (localStorage.getItem(hour)) { // alert message when data is saved to local storage.
             alert(`you have saved ${txt} at ${hour}`);
         };
     });
 
-function currentDay() {
+function currentDay() { // date and time operated functions.
     $('#currentDay').text(moment().format('dddd MMMM Do, YYYY'));
     var currentTime = moment().hours();
     $('.desciption').each(function(){
         var listHours = parseInt($(this).attr('id').substr(0));
 
-        if (listHours > currentTime) {
+        if (listHours > currentTime) { // sets class of future for hours that have not occured.
             $(this).addClass('future')
         }
-        if (listHours < currentTime) {
+        if (listHours < currentTime) { // sets class of past to hours that have occured.
             $(this).removeClass('present')
             $(this).addClass('past')
         }
-        if (listHours === currentTime) {
+        if (listHours === currentTime) { // sets class of present to the current hour.
             $(this).removeClass('past')
             $(this).addClass('present')
         } 
@@ -42,7 +42,8 @@ function currentDay() {
 
 currentDay();
 
-$('#9AM').val(localStorage.getItem('9AM'));
+//if there is local storage that has a key equal to the id the saved text.
+$('#9AM').val(localStorage.getItem('9AM')); 
 $('#10AM').val(localStorage.getItem('10AM'));
 $('#11AM').val(localStorage.getItem('11AM'));
 $('#12PM').val(localStorage.getItem('12PM'));
@@ -57,20 +58,14 @@ $('#18PM').val(localStorage.getItem('18PM'));
 
 
 
-// if else checks checking current hour with listHours. 
 
 
+
+// -------------------------sandbox and method -------------------------------//
 
 // create the layout of time in html.
 
 // create hourly time blocks.
-
-
-// function renderLastEntry () {
-//     var textSaved = $($(this).attr('id'));
-// }
-
-
 
 // make past timeblocks grey using .past class. use the var.addClass('past')
 
@@ -80,16 +75,8 @@ $('#18PM').val(localStorage.getItem('18PM'));
 
 // make the time blocks functional so an event can be entered into them.
 
-
-
-// console.log(form);
-
 // create a save button for each timeblock that saves the event data to local storage.
 
 // when the page refreshes the timeblocks data remains in the block.
 
-
-
-
 // create var for each row.
-
---
+++ README.md
@@ -0,0 +1,33 @@
+# Work Day Calendar
+
+## Link to Deployed Application
+
+https://brettgillund.github.io/Work-Day-Calendar/
+
+## Contributors
+
+Brett Gillund
+
+## Description
+
+Everyone needs a way to stay organized! Whether that means keeping track of the days meetings, scheduling time for studying, or even just setting reminders to take a lunch, this applicaiton has what a user needs to keep their days schedule on track! The Work Day Calendar keeps track of the current time using Moment.js, and color coordinates each input box according to the current time Please see the funcionality section below for further information.
+
+## Technologies Used
+
+This applicaiton is a work day calendar that uses HTML, CSS, JavaScript, and Moment.js elements for function and styling. 
+
+## Functionality
+
+The Work Day Calendar applicaiton uses Moment.js to track the current time and date. It displays this date in the top section of the browser. Additionally, depending on the current time, the input boxes will display a given color. Please see the below table for further information. Lastly, the applicaiton saves user input to your devices local storage upon clicking the respective sections save buttion.
+
+Past - Grey
+
+Present - Red
+
+Future - Green
+
+## Contact information
+
+Brett Gillund
+(612)408-4708
+BrettGillund2@Gmail.com
